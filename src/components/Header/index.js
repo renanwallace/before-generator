3455a7dc0d58bbeaf75bf72c18189e2ec70262c0
renanwallace <aquino180@gmail.com>
2020-02-10 13:35:30 -0400

    remove rota não finalizada do header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,27 +1,31 @@
 /*global chrome*/
-import React, { memo } from "react";
-import { Link } from "react-router-dom";
+import React, { memo } from 'react';
+import { Link } from 'react-router-dom';
 import { Container, Nav, RouteList, Title } from './styles';
 
 function Header() {
   const routes = [
-    { name: "CPF/CNPJ", path: "/" },
-    { name: "Pessoa", path: "/pessoa" }
+    { name: 'CPF/CNPJ', path: '/' },
+    // { name: "Pessoa", path: "/pessoa" }
   ];
 
   const Route = ({ route }) => (
     <li>
-      <Link className="link" to={route.path}>{route.name}</Link>
+      <Link className="link" to={route.path}>
+        {route.name}
+      </Link>
     </li>
   );
 
   const openBefore = () => {
     chrome.tabs.create({ url: 'https://before.com.br/', active: false });
-  }
+  };
 
   return (
     <Container>
-      <Title onClick={openBefore} className="link">Before</Title>
+      <Title onClick={openBefore} className="link">
+        Before
+      </Title>
       <Nav>
         <RouteList>
           {routes.map((route, index) => (
